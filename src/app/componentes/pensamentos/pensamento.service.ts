916e74ed2ad99432bdaf4063a27ee9f4c3e3133f
Andre <andreluis0015@gmail.com>
2023-07-09 01:06:45 -0300

    + service pensamento | crud

---
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Pensamento } from './pensamento';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class PensamentoService {
+
+  private readonly API = 'http://localhost:3000/pensamentos';
+
+  constructor(private httpClient: HttpClient) { }
+
+  listar(): Observable<Pensamento[]> {
+    return this.httpClient.get<Pensamento[]>(this.API);
+  }
+
+  buscarPorId(id: number): Observable<Pensamento> {
+    const url = `${this.API}/${id}`
+    return this.httpClient.get<Pensamento>(url)
+  }
+
+  criar(pensamento: Pensamento): Observable<Pensamento> {
+    return this.httpClient.post<Pensamento>(this.API, pensamento);
+  }
+
+  excluir(id: number): Observable<Pensamento> {
+    const url = `${this.API}/${id}`
+    return this.httpClient.delete<Pensamento>(url)
+  }
+
+  editar(pensamento: Pensamento): Observable<Pensamento> {
+    const url = `${this.API}/${pensamento.id}`
+    return this.httpClient .put<Pensamento>(url, pensamento)
+  }
+
+}
